911b77efb3c0ba3a8aaf8a5b224415308b28739d
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-03-21 01:36:46 +0300

    codbasew

--- models/user.py
+++ models/user.py
@@ -7,15 +7,10 @@ from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-    __tablename__ = "users"
+    """User class inherits from BaseModel and Base"""
+    __tablename__ = 'users'
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
