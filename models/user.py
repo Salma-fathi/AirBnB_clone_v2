3c154f186494bb59dc48f627e9138e549d7945dc
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-03-20 23:24:35 +0300

    usertask

--- models/user.py
+++ models/user.py
@@ -4,8 +4,13 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    """This class defines a user by various attributes,Represents a user for a MySQL database.
+
+    Inherits from SQLAlchemy Base and links to the MySQL table users.
+"""
+
+    __tablename__ = "users"
+    email = Column(String(128), nullable =False)
+    password = Column(String(128), nullable =False)
+    first_name = Column(String(128), nullable =False)
+    last_name = Column(String(128), nullable =False)
