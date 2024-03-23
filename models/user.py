7157c66534bda0008b245eea8d104a198f7e233d
Salma-fathi <67196308+Salma-fathi@users.noreply.github.com>
2024-03-23 05:31:43 +0300

    replacessss

--- models/user.py
+++ models/user.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from sqlalchemy.ext.declarative import declarative_base
+"""This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
-    """User class inherits from BaseModel and Base"""
-    __tablename__ = 'users'
-
+    """ This class defines a user by various attributes """
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
